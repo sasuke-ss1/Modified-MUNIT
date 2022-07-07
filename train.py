209eeabe1675b9b40944c686e83190ea49606839
vm <sahivanshu123@gmail.com>
2022-07-07 22:47:31 +0530

    yes

--- train.py
+++ train.py
@@ -1,27 +1,40 @@
+from py import process
 import DA_net.utils as utils
 from argparse import ArgumentParser
 import DA_net.S_reconstruction as Srecon
 import DA_net.R_reconstruction as Rrecon
 import DA_net.End2End as End2End
 import torch
-log = open("./checkpoints/S_log.txt", 'a')
+import os
+from time import process_time
+
+
 iterations = 0
-def train(opt):
+def train(opt, log):
     fake = utils.Folder_data(opt.fake, opt.batch_size, train=False)
     clear = utils.Folder_data(opt.clear, opt.batch_size, train=False)
     real = utils.Folder_data(opt.real, opt.batch_size, train=True)
     iterations = 0
+    tmp = [("S_weights", "Srecon", "S_images"), ("R_weights", "Rrecon", "R_images"), ("e2e_weights", "e2e_recon", "e2e_images")]
+    if opt.model == 0:
+        model = Srecon.S_recon(opt.m_config, opt.res_config, opt.d_config).cuda()
+    elif opt.model == 1:
+        model = Rrecon.R_recon(opt.m_config, opt.res_config, opt.d_config).cuda()
+    elif opt.model == 2:
+        model = End2End.End2End(opt.m_config, opt.res_config, opt.d_config, opt.e2eConfig)
+    else:
+        raise NotImplementedError
     while True:
         for it, (fake_img, real_img, clear_img) in enumerate(zip(fake, real, clear)):
-            model = Srecon.S_recon(opt.m_config, opt.res_config, opt.d_config).cuda()
+            start_time = process_time()
             model.optimize(fake_img, real_img, clear_img)
             iterations += 1
+            print(f"{iterations} Elapsed Time: {process_time() - start_time}")
             if(not iterations % opt.save_image):
-                utils.save_images(model.r2s_recon_img.detach(), model.s_recon_img.detach(), fake_img.detach(), real_img.detach(), clear_img.detach(), iterations)
-                log.write(f"{iterations}  G: {model.netLoss}   D:{model.loss_D}")
-                
-                #print(model.s_recon_img.shape, model.r2s_recon_img.shape )
-                torch.save(model.state_dict(), f"./checkpoints/S_weight/Srecon_{iterations}.pth")
+                model.save(fake_img, real_img, clear_img, f"./checkpoints/{tmp[opt.model][2]}/{iterations}.png")
+                torch.save(model.state_dict(), f"./checkpoints/{tmp[opt.model][0]}/{tmp[opt.model][1]}_{iterations}.pth")
+            if(not iterations % opt.print_log):
+                print(f"{iterations}  G: {model.netLoss}   D:{model.loss_D}", file = log)
             if(iterations >= opt.max_iter):
                 return
                 
@@ -35,14 +48,23 @@ parser.add_argument('--clear', type=str, default='/home/neham/uw_datasets/UWCNN_
 parser.add_argument('--output_path', type=str, default='/home/intern/ss_sasuke/outputs/', help="outputs path")
 parser.add_argument('--batch_size', type=int, default=1, help='batch_size')
 parser.add_argument('--max_iter', type=int, default=100000, help="max iterations")
-parser.add_argument('--save_image', type=int, default=5000, help='save images iterations')
+parser.add_argument('--save_image', type=int, default=5, help='save images iterations')
+parser.add_argument("--print_log", type=int, default=2, help ="iteration to save log")
 parser.add_argument('--m_config', type=str, default='/home/intern/ss_sasuke/Modified-MUNIT/MUNIT/config.yaml', help='Path to config files for MUNIT.')
 parser.add_argument('--res_config', type=str, default='/home/intern/ss_sasuke/Modified-MUNIT/DA_net/res_config:.yml', help='Path to config files for Resnet.')
 parser.add_argument('--d_config', type=str, default='/home/intern/ss_sasuke/Modified-MUNIT/DA_net/D_config.yml', help='Path to config files for Discriminator.')
 parser.add_argument("--e2eConfig", type=str, default="/home/intern/ss_sasuke/Modified-MUNIT/DA_net/e2eConfig.yml", help="Path to config for End2End")
 opts = parser.parse_args()
 
-train(opts)
+paths = [("S_images", "S_weights", "S_log"), ("R_images", "R_weights", "R_log"), ("e2e_images", "e2e_weights", "e2e_log")]
+
+try:
+    os.mkdir(f"./checkpoints/{paths[opts.model][0]}")
+    os.mkdir(f"./checkpoints/{paths[opts.model][1]}")    
+except FileExistsError:
+    pass
+log = open(f"./checkpoints/{paths[opts.model][2]}.txt", 'a',buffering = 1)
+train(opts, log)
 
 
     
